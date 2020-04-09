46c5ae1b7db75516607829cbd4da155fef5b0c5e
shohan <shohanduttaroy99@gmail.com>
2020-04-10 01:25:12 +0530

    [feat] Updated website

--- templates/service-worker.js
+++ templates/service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.e37bd76c11b5d5bb9d7c5a149b5f544d.js"
+  "/precache-manifest.6ad43f10366de6028aac8018cc1e53fb.js"
 );
 
 self.addEventListener('message', (event) => {
